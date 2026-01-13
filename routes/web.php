3589ca79fcb339d2ec6e83ade1410270b8a8f742
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2026-01-13 20:53:26 +0100

    removed trash routes from starter kit

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,11 @@
 use App\Http\Controllers\GistController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return Inertia::render('welcome');
 })->name('home');
 
 Route::get('/gist/{gist}', [GistController::class, 'show']);
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
-
-require __DIR__ . '/settings.php';
+// require __DIR__ . '/settings.php';
