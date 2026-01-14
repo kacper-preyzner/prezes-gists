310d04378fea93296d42eada66addc634809b7ff
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2026-01-14 01:25:52 +0100

    added pre-commit and cleand shit from starter kit

--- resources/js/components/app-logo-icon.tsx
+++
@@ -1,6 +0,0 @@
-import { Code2 } from 'lucide-react';
-import { SVGAttributes } from 'react';
-
-export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
-    return <Code2 {...props} className="text-blue-400" />;
-}
